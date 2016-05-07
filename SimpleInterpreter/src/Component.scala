ca878d69ed94fa1a9f0d2e9d7ce5226b2f1dcef2
amaximov <amaximov@student.ethz.ch>
2016-05-08 00:29:44 +0200

    Remove Scala files

--- SimpleInterpreter/src/Component.scala
+++
@@ -1,8 +0,0 @@
-/**
-  * Created by Maximova on 27.01.2016.
-  */
-abstract class Component {
-  val name: String
-  val arity: Int
-  def eval(args: List[Any])
-}
--- SimpleInterpreter/src/Program.scala
+++
@@ -1,7 +0,0 @@
-/**
-  * Created by Maximova on 27.01.2016.
-  */
-sealed abstract class Program
-  case class Var(name: String)
-  case class Const(value: AnyVal)
-  case class Fun(component: Component, args: List[Program])
