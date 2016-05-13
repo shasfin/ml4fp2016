7c139b4fd6078552fd041da4693f8f896829d52f
Alexandra Maximova <amaximov@ethz.ch>
2016-05-13 18:33:26 +0200

    Test skeleton. No code yet.
    Difficulty: how to create a library.

---
+++ baseline/zil/t/test_synthesizer.ml
@@ -0,0 +1,20 @@
+open TestSimple;;
+
+open Zil.Lambda;;
+open Zil.Parse;;
+open Zil;;
+
+plan 1;; (* what does this mean? *)
+
+(******************************************************************************)
+(* Define library *)
+
+(* TODO think on how to get typed terms, you need a typed library *)
+(* Type inference relies on the presence of two libraries... *)
+
+(******************************************************************************)
+(* Prepare library for unification *)
+
+
+(******************************************************************************)
+(* Generate some simple programs (only type-based, without i/o *)
