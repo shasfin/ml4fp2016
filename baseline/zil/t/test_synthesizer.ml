26c2feba79765273d2188a35693262c5b8cfa6b2
amaximov <amaximov@student.ethz.ch>
2016-05-22 16:02:36 +0200

    Implement sum
    Attention, the enumeration stuff is commented out.
    Debugging mess still everywhere.

--- baseline/zil/t/test_synthesizer.ml
+++ baseline/zil/t/test_synthesizer.ml
@@ -5,7 +5,7 @@ open Zil.Lambda;;
 open Zil.Parse;;
 open Zil;;
 
-plan 5;;
+plan 9;;
 (******************************************************************************)
 (* Define library *)
 
@@ -19,13 +19,11 @@ let sym_lib = Library.create ();;
 (* Recursive lists *)
 let list_sig = 1;;
 let list_def = parse_type "@ #0 -> (#1 -> List #1 -> #0) -> #0";;
-
 let () = Library.add_type "List" list_def list_sig sym_lib;;
 
 (* Natural numbers *)
 let nat_sig = 0;;
 let nat_def = parse_type "@ #0 -> (Nat -> #0) -> #0";;
-
 let () = Library.add_type "Nat" nat_def nat_sig sym_lib;;
 
 (* Terms *)
@@ -33,59 +31,77 @@ let () = Library.add_type "Nat" nat_def nat_sig sym_lib;;
 let list_nil_sig = parse_type "@ List #0";;
 let list_nil_def = parse_term "* * { [#0] [#1 -> List #1 -> #0] : $1 }";;
 let list_nil_fun = name "nil" (eval list_nil_def);;
-
 let () = Library.add_term "nil" list_nil_fun list_nil_sig sym_lib;;
 
 let list_con_sig = parse_type "@ #0 -> List #0 -> List #0";;
 let list_con_def = parse_term "* { [#0] [List #0] : * { [#0] [#1 -> List #1 -> #0] : $0 $3 $2 } }";;
 let list_con_fun = name "con" (eval list_con_def);;
-
 let () = Library.add_term "con" list_con_fun list_con_sig sym_lib;;
 
 (* nat constructors *)
 let nat_zero_sig = parse_type "Nat";;
 let nat_zero_def = parse_term "* { [#0] [Nat -> #0] : $1 }";;
 let nat_zero_fun = name "zero" (eval nat_zero_def);;
-
 let () = Library.add_term "zero" nat_zero_fun nat_zero_sig sym_lib;;
 
 let nat_succ_sig = parse_type "Nat -> Nat";;
 let nat_succ_def = parse_term "{ [Nat] : * { [#0] [Nat -> #0] : $0 $2 } }";;
 let nat_succ_fun = name "succ" (eval nat_succ_def);;
-
 let () = Library.add_term "succ" nat_succ_fun nat_succ_sig sym_lib;;
 
 (* functions *)
 let list_map_sig = parse_type "@ @ (#1 -> #0) -> List #1 -> List #0";;
 let list_map_def = parse_term "* * { [#1 -> #0] [List #1] : $0 #0 (nil #0) { [#1] [List #1] : con #0 ($3 $1) (map #1 #0 $3 $0) } }";;
 let list_map_fun = name "map" (eval list_map_def);;
-
 let () = Library.add_term "map" list_map_fun list_map_sig sym_lib;;
+
+let list_sum_sig = parse_type "List Nat -> Nat";;
+let list_sum_def = parse_term "{ [List Nat] : $0 Nat zero { [Nat] [List Nat] : add $1 (sum $0) } }";;
+let list_sum_fun = name "sum" (eval list_sum_def);;
+let () = Library.add_term "sum" list_sum_fun list_sum_sig sym_lib;;
 
 let nat_add_sig = parse_type "Nat -> Nat -> Nat";;
 let nat_add_def = parse_term "{ [Nat] [Nat] : $1 Nat $0 { [Nat] : succ (add $0 $1) } }";;
 let nat_add_fun = name "add" (eval nat_add_def);;
-
 let () = Library.add_term "add" nat_add_fun nat_add_sig sym_lib;;
 
 
 (******************************************************************************)
 (* Test some of the library functions *)
+let sym_def = Library.get_lib_def sym_lib;;
 
+(* syntax sugar for nats and lists of nats *)
+let rec number_to_nat n = match n with
+  | 0 -> "zero"
+  | 1 -> "succ zero"
+  | n -> sprintf "succ (%s)" (number_to_nat (n-1))
+	
+let rec list_to_list xs = match xs with
+  | [] -> "nil Nat"
+  | x::xs -> sprintf "con Nat (%s) (%s)" (number_to_nat x) (list_to_list xs)
+
+(* test addition *)
 let test_add n m msg =
-  let rec number_to_nat n = match n with
-    | 0 -> "zero"
-    | 1 -> "succ zero"
-    | n -> sprintf "succ (%s)" (number_to_nat (n-1)) in
-  let got = eval ~sym_def:(Library.get_lib_def sym_lib) (parse_term (sprintf "add (%s) (%s)" (number_to_nat n) (number_to_nat m))) in
+  let got = eval ~sym_def:sym_def (parse_term (sprintf "add (%s) (%s)" (number_to_nat n) (number_to_nat m))) in
   is (Term.to_string got) (number_to_nat (n+m)) msg
 
-let test0_0 = test_add 0 0 "0+0";;
-let test1_0 = test_add 1 0 "1+0";;
-let test0_1 = test_add 0 1 "0+1";;
-let test3_5 = test_add 1 1 "1+1";;
-let test2_3 = test_add 2 3 "2+3";;
+let test_add_0_0 = test_add 0 0 "0+0";;
+let test_add_1_0 = test_add 1 0 "1+0";;
+let test_add_0_1 = test_add 0 1 "0+1";;
+let test_add_3_5 = test_add 1 1 "1+1";;
+let test_add_2_3 = test_add 2 3 "2+3";;
 
+(* test sum *)
+let test_sum xs msg =
+  let got = eval ~sym_def:sym_def (parse_term (sprintf "sum (%s)" (list_to_list xs))) in
+  is (Term.to_string got) (number_to_nat (List.fold_left (+) 0 xs)) msg
+  
+let test_sum1 = test_sum [] "sum []";;
+let test_sum2 = test_sum [2] "sum [2]";;
+let test_sum3 = test_sum [1;2;3] "sum [1;2;3]";;
+let test_sum4 = test_sum [2;3;5;1] "sum [2;3;5;1]";;
+
+(*
 (******************************************************************************)
 (* Prepare library for unification *)
 let first_prog = Program.create ();;
@@ -197,3 +213,4 @@ let map_test_2 =
 let () = print_string (String.concat "\n" (List.map Program.to_string map_test_2));;
 
 
+*)
